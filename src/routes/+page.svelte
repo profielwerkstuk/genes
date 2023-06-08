876549e0172eef45752fc1c19f0140f2c07bb714
Maurits Wilke <mauritswilke@gmail.com>
2023-06-08 07:25:36 +0200

    feat: :sparkles: added a way to load other genomes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import Matter, { Body, Constraint, Common } from "matter-js";
+    import Matter, { Body, Constraint, Common, World } from "matter-js";
 
     const SETTINGS = {
         neuronSize: 20,
@@ -35,7 +35,7 @@
 
     onMount(async () => {
         const res = await fetch("/genomes/bram.json");
-        const GENOME = (await res.json()) as Genome;
+        let GENOME = (await res.json()) as Genome;
 
         const Engine = Matter.Engine,
             Render = Matter.Render,
@@ -56,73 +56,94 @@
             },
         });
 
-        const ground = Bodies.rectangle(window.innerWidth / 2, window.innerHeight, window.innerWidth, 50, { isStatic: true });
-        const ceiling = Bodies.rectangle(window.innerWidth / 2, 0, window.innerWidth, 50, { isStatic: true });
-        const leftWall = Bodies.rectangle(0, window.innerHeight / 2, 50, window.innerHeight, { isStatic: true });
-        const rightWall = Bodies.rectangle(window.innerWidth, window.innerHeight / 2, 50, window.innerHeight, { isStatic: true });
-        Composite.add(engine.world, [ground, ceiling, leftWall, rightWall]);
+        let allBodies: (Body | Constraint)[] = [];
+        function renderFunction() {
+            const ground = Bodies.rectangle(window.innerWidth / 2, window.innerHeight, window.innerWidth, 50, { isStatic: true });
+            const ceiling = Bodies.rectangle(window.innerWidth / 2, 0, window.innerWidth, 50, { isStatic: true });
+            const leftWall = Bodies.rectangle(0, window.innerHeight / 2, 50, window.innerHeight, { isStatic: true });
+            const rightWall = Bodies.rectangle(window.innerWidth, window.innerHeight / 2, 50, window.innerHeight, { isStatic: true });
+            Composite.add(engine.world, [ground, ceiling, leftWall, rightWall]);
 
-        const nodes: { [key: string]: Body } = {};
-        const lines: Constraint[] = [];
+            const nodes: { [key: string]: Body } = {};
+            const lines: Constraint[] = [];
 
-        for (const node of GENOME.nodes) {
-            const Y = Math.floor(0.5 * window.innerHeight);
-            const X = Math.floor(0.5 * window.innerWidth);
+            for (const node of GENOME.nodes) {
+                const Y = Math.floor(0.5 * window.innerHeight);
+                const X = Math.floor(0.5 * window.innerWidth);
 
-            nodes[node.id] = Bodies.circle(X, Y, SETTINGS.neuronSize, {
-                frictionAir: 1,
-                render: {
-                    fillStyle: node.active ? SETTINGS.colourKeys[node.type].fillStyle : SETTINGS.colourKeys[node.type].fillStyle,
-                    strokeStyle: node.active ? SETTINGS.colourKeys[node.type].strokeStyle : "red",
-                    lineWidth: 2,
-                },
-            });
+                nodes[node.id] = Bodies.circle(X, Y, SETTINGS.neuronSize, {
+                    frictionAir: 1,
+                    render: {
+                        fillStyle: node.active ? SETTINGS.colourKeys[node.type].fillStyle : SETTINGS.colourKeys[node.type].fillStyle,
+                        strokeStyle: node.active ? SETTINGS.colourKeys[node.type].strokeStyle : "red",
+                        lineWidth: 2,
+                    },
+                });
+            }
+
+            for (const connection of GENOME.connections) {
+                const line = Constraint.create({
+                    bodyA: nodes[connection.inputNode.id],
+                    bodyB: nodes[connection.outputNode.id],
+                    length: Math.abs(connection.weight) * 650,
+                    stiffness: 0.8,
+                    render: {
+                        strokeStyle: `#FFFFFF`,
+                        lineWidth: Math.abs(connection.weight) * 1,
+                    },
+                });
+
+                lines.push(line);
+            }
+
+            allBodies.push(...Object.values(nodes));
+            allBodies.push(...lines);
+
+            Composite.add(engine.world, allBodies);
+
+            Render.run(render);
+
+            const runner = Runner.create();
+            Runner.run(runner, engine);
+
+            function applyRandomForce(bodies: Body[]) {
+                const forceMagnitude = Common.random(0.03, 0.08);
+                const forceAngle = Common.random(0, 2 * Math.PI);
+
+                const forceX = Math.cos(forceAngle) * forceMagnitude;
+                const forceY = Math.sin(forceAngle) * forceMagnitude;
+
+                const body = bodies[Math.floor(Math.random() * bodies.length)];
+                Body.applyForce(body, body.position, { x: forceX, y: forceY });
+
+                const interval = Common.random(100, 500);
+                setTimeout(() => applyRandomForce(bodies), interval);
+            }
+            applyRandomForce([...Object.values(nodes)]);
         }
 
-        for (const connection of GENOME.connections) {
-            const line = Constraint.create({
-                bodyA: nodes[connection.inputNode.id],
-                bodyB: nodes[connection.outputNode.id],
-                length: Math.abs(connection.weight) * 650,
-                stiffness: 0.8,
-                render: {
-                    strokeStyle: `#FFFFFF`,
-                    lineWidth: Math.abs(connection.weight) * 1,
-                },
-            });
+        renderFunction();
 
-            lines.push(line);
-        }
+        addEventListener("keypress", (e) => {
+            if (e.key === "l") {
+                const input = JSON.parse(prompt("Paste the brain:")!);
+                GENOME = input as unknown as Genome;
 
-        const allBodies = [];
-        allBodies.push(...Object.values(nodes));
-        allBodies.push(...lines);
+                World.clear(engine.world, false);
+                Engine.clear(engine);
 
-        Composite.add(engine.world, allBodies);
+                allBodies = [];
 
-        Render.run(render);
+                renderFunction();
+            }
 
-        const runner = Runner.create();
-        Runner.run(runner, engine);
-
-        function applyRandomForce(bodies: Body[]) {
-            // Generate random force values
-            const forceMagnitude = Common.random(0.03, 0.08); // Random magnitude between 0.005 and 0.02
-            const forceAngle = Common.random(0, 2 * Math.PI); // Random angle between 0 and 2 * PI
-
-            // Calculate force components
-            const forceX = Math.cos(forceAngle) * forceMagnitude;
-            const forceY = Math.sin(forceAngle) * forceMagnitude;
-
-            const body = bodies[Math.floor(Math.random() * bodies.length)];
-            // Apply the force to the body
-            Body.applyForce(body, body.position, { x: forceX, y: forceY });
-
-            // Schedule the next random force
-            const interval = Common.random(100, 500); // Random interval between 500 and 2000 milliseconds
-            setTimeout(() => applyRandomForce(bodies), interval);
-        }
-        applyRandomForce([...Object.values(nodes)]);
+            if (e.key === "f") {
+                allBodies.forEach((v) => {
+                    // @ts-ignore This is the way to do it
+                    if (v.type === "body") v.isStatic = !v.isStatic;
+                });
+            }
+        });
     });
 </script>
 
